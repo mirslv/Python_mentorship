c8e0ed3e833eceed78f4de11bb35d23cad046666
mirslv <mirslv@gmail.com>
2017-03-17 17:31:09 +0200

    Page object test

--- PO_1.py
+++ PO_1.py
@@ -6,30 +6,28 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-class PythonOrgSearch(unittest.TestCase):
-    def setUp(self):
-        self.driver = webdriver.Chrome()
-        self.driver.get("http://www.jysk.ua")
+class verifyResults(unittest.TestCase):
 
     def test_search_in_jysk_ua(self):
-        main_page = MainPage(self.driver)
-        main_page.search_text_element
-        main_page.click_go_button
-        assert main_page.check_results
-        main_page.test_matchCount
+        self.setUp()
+        main_page = MainPage()
+        main_page.search_text_element()
+        main_page.click_go_button()
+        main_page.check_results()
 
 	def tearDown(self):
 		self.driver.close()
 
 
-class BasePage(object):
-    def __init__(self, driver):
-        self.driver = driver
+class BasePage:
+    def __init__(self):
+        self.driver = webdriver.Chrome()
+        self.driver.get("http://www.jysk.ua")
 
 
 class MainPage(BasePage):
     def search_text_element(self):
-        inputElement = driver.find_element_by_name("query")
+        inputElement = self.driver.find_element_by_name("query")
         inputElement.send_keys("RYSLINGE")
 
     def click_go_button(self):
@@ -41,10 +39,8 @@ class MainPage(BasePage):
         xpathResult = self.driver.find_element_by_xpath("//div[@class = 'view-header']/h1/span")
         result = xpathResult.text
         print 'Count of RYSLINGE = ' + result
-        assert int(result) == 9
-
-    def test_matchCount():
-        assert int(xpathResult.text) == 8
+        print 'ssss'
+        assert (int(result) == 8)
 
 
 #if __name__ == "__main__":
